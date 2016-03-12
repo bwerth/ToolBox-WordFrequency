87d190faa294d560d6a4fe8d35c9eb971ab2bc3a
bwerth <bryan.werth@students.olin.edu>
2016-03-12 18:15:18 -0500

    Finished this toolbox

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # ToolBox-WordFrequency
-Word Frequency Analysis Project Toolbox starter code
-
-Full instructions at https://sites.google.com/site/sd16spring/home/project-toolbox/word-frequency-analysis
+For this assignment, I found the 100 most common words in David Copperfield. I changed the suggested starting code for cutting out the header comments because I did not like the line list format. Other than that, everything should be fairly standard.
--- frequency.py
+++ frequency.py
@@ -9,7 +9,18 @@ def get_word_list(file_name):
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	f = open(file_name,'r')
+	text = f.read()
+	(before,text) = text.split('START OF THIS PROJECT GUTENBERG EBOOK')
+	text = text.translate(None,string.punctuation)
+	text = text.lower()
+	return text.split()
+
+def get_word_histogram(word_list):
+	hist = dict()
+	for word in word_list:
+		hist[word] = hist.get(word,0)+1;
+	return hist
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -21,4 +32,17 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequentlyoccurring
 	"""
-	pass
+	hist = get_word_histogram(word_list)
+	histlist = hist.items()
+	sorted_hist = sorted(histlist, key = lambda tuple: tuple[1])
+	sorted_hist = sorted_hist[::-1]
+	i = 0
+	sorted_list = []
+	while i < n:
+		sorted_list.append(sorted_hist[i][0])
+		i = i + 1
+	return sorted_list
+
+word_list = get_word_list('davidcopperfield.txt')
+sorted_list =  get_top_n_words(word_list,100)
+print sorted_list
